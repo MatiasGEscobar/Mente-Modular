d3284931619cbc1100b3a1d355ba0546fda7679c
MatiasGEscobar <matiie9135@gmail.com>
2025-06-22 11:52:13 -0300

    servidor iniciado back

---
+++ back/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const server = express();
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
